253857f86b9d647e241c8f18f16292f488c02b23
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-05-22 13:37:57 +0300

    setup dependencies and initialize working sample

--- Apache-Kafka-Basics/build.gradle.kts
+++ Apache-Kafka-Basics/build.gradle.kts
@@ -10,8 +10,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.7.0")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    testImplementation("org.slf4j:slf4j-simple:2.0.13")
+
 }
 
 tasks.test {
