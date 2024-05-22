ef7306b26f7be1fb2bef6c3b5ea228cd5e0100c7
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-05-22 13:48:00 +0300

    add Logger implementation

--- Apache-Kafka-Basics/build.gradle.kts
+++ Apache-Kafka-Basics/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.13")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
-    testImplementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
 
 }
 
--- Apache-Kafka-Basics/src/main/java/yevhent/demo/kafka/ProducerDemo.java
+++ Apache-Kafka-Basics/src/main/java/yevhent/demo/kafka/ProducerDemo.java
@@ -1,7 +1,14 @@
 package yevhent.demo.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerDemo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerDemo.class.getName());
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        LOGGER.info("Hello world!");
     }
 }
